e3c40eefb83fdbcd6189377c42f174c87e635fc6
alex-f6668fed <78003577+alex-f6668fed@users.noreply.github.com>
2023-03-24 20:56:03 -0400

    Add index exports

--- packages/intrinsics/src/lib/index.ts
+++ packages/intrinsics/src/lib/index.ts
@@ -1,4 +1,10 @@
 export { default as Divider } from "./components/Divider";
 export { default as MaterialIcon } from "./components/MaterialIcon";
+export { default as MaterialSymbol } from "./components/MaterialSymbol";
 export { default as Panel } from "./components/Panel";
 export { default as Shape } from "./components/Shape";
+
+export { default as classNames } from "./classnames";
+
+export { default as useStylesheet } from "./hooks/useStylesheet";
+export { default as useStyles } from "./hooks/useStyles";
